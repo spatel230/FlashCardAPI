743f1127d27a2ed808d80b86bfb7504493cea8c3
Shrey Patel <shreyppatel97@gmail.com>
2021-04-23 11:00:28 -0400

    Login

--- src/main/java/com/example/demo/dao/LoginDAO.java
+++ src/main/java/com/example/demo/dao/LoginDAO.java
@@ -1,4 +1,12 @@
 package com.example.demo.dao;
 
+import com.example.demo.entity.User;
+
+import java.util.List;
+
 public interface LoginDAO {
+    List<User> findAllUser();
+    User findById(int ID);
+    void save(User newUser);
+    void deleteUserById(int ID);
 }
--- src/main/java/com/example/demo/dao/LoginIMPL.java
+++ src/main/java/com/example/demo/dao/LoginIMPL.java
@@ -1,4 +1,45 @@
 package com.example.demo.dao;
 
-public class LoginIMPL {
+import com.example.demo.entity.User;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+@Repository
+public class LoginIMPL implements LoginDAO{
+    private final EntityManager entityManager;
+
+    public LoginIMPL(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public List<User> findAllUser() {
+        Session cSession=entityManager.unwrap(Session.class);
+        Query<User> myQuery=cSession.createQuery("from User");
+        return myQuery.getResultList();
+    }
+
+    @Override
+    public User findById(int ID) {
+        Session cSession=entityManager.unwrap(Session.class);
+        return cSession.get(User.class, ID);
+    }
+
+    @Override
+    public void save(User newUser) {
+        Session cSession=entityManager.unwrap(Session.class);
+        cSession.saveOrUpdate(newUser);
+    }
+
+    @Override
+    public void deleteUserById(int ID) {
+        Session cSession=entityManager.unwrap(Session.class);
+        User deleteUser=cSession.get(User.class, ID);
+        cSession.delete(deleteUser);
+    }
 }
